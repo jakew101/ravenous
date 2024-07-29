a33bcbd79791cdea0d74949bbb1a507a0c5cfe82
Jacob Wooldredge <jacob.wooldredge@emi-rs.com>
2024-07-29 11:56:41 -0400

    Add states to searchbar component to handle data passed to Yelp API

--- src/components/searchbar/searchbar.js
+++ src/components/searchbar/searchbar.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styles from './searchbar.module.css';
 
 function SearchBar() {
+    const [search, setSearch] = useState('');
+    const [location, setLocation] = useState('');
+    const [filter, setFilter] = useState('');
+
+    const handleFilterChange = (event) => {
+        setFilter(event.target.getAttribute('data-filter'));
+    }
+
+    const handleSearchChange = (event) => {
+        setSearch(event.target.value);
+    }
+
+    const handleLocationChange = (event) => {
+        setLocation(event.target.value);
+    }
+
+    const handleSearch = () => {
+        console.log(`Searching Yelp with ${search}, ${location}, ${filter}`);
+    }
+
     return (
         <div>
-            <div className={Styles.filterOptions}>
-                <button>Best Match</button>
-                <button>Highest Rated</button>
-                <button>Most Reviewed</button>
-            </div>
+            <ul className={Styles.filterOptions}>
+                <li key='best_match' data-filter='best_match' onClick={handleFilterChange} className = {filter === 'best_match' ? Styles.filterOptionsActive : ''}>Best Match</li>
+                <li key='rating' data-filter='rating' onClick={handleFilterChange} className = {filter === 'rating' ? Styles.filterOptionsActive : ''}>Highest Rated</li>
+                <li key='review_count' data-filter='review_count' onClick={handleFilterChange} className = {filter === 'review_count' ? Styles.filterOptionsActive : ''}>Most Reviewed</li>
+            </ul>
             <div className={Styles.searchBar}>
-                <input type="text" placeholder="Search Businesses" />
-                <input type="text" placeholder="Where?" />
+                <input type="text" placeholder="Search Restaurants..." value={search} onChange={handleSearchChange} />
+                <input type="text" placeholder="Where?" value={location} onChange={handleLocationChange} />
             </div>
-            <button className={Styles.button}>Let's Eat</button>
+            <button className={Styles.button} onClick={handleSearch}>Let's Eat</button>
         </div>
     );
 }
